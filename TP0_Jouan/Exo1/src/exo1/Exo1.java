49d3b494a17d3eaf99257562495858677050d12a
Félix Jouan <felix@10.3.60.194>
2024-09-23 18:23:36 +0200

    voici mon Tp0 ! version2

---
+++ TP0_Jouan/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ * Jouan
+ *TDA   
+ *Septembre 2024
+*/
+
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author felix    
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");// TODO code application logic here
+        System.out.println("au revoir");
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        sc.nextLine();
+    }
+    
+}
