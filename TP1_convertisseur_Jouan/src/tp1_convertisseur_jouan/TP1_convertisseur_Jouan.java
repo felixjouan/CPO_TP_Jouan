372ddb93f5ffc685dc66b75ac70a4c936d349b9a
Félix Jouan <felix@10.3.60.194>
2024-09-27 17:10:50 +0200

    TP1 first version

---
+++ TP1_convertisseur_Jouan/src/tp1_convertisseur_jouan/TP1_convertisseur_Jouan.java
@@ -0,0 +1,29 @@
+/*
+ * Jouan
+ *TP1
+ */
+package tp1_convertisseur_jouan;
+
+import static java.time.Clock.system;
+import static java.time.InstantSource.system;
+import java.util.Scanner;
+
+/**
+ *
+ * @author felix
+ */
+public class TP1_convertisseur_Jouan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Veuillez saisir la première tempréature: ");
+    double temp1 = sc.nextDouble(); 
+    
+     
+     
+    }
+    
+}
